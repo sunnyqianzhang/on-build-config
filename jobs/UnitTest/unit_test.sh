397e720556d28ddaf42b65b88ed3d27bcac291de
sunnyzhang <sunny.zhang@emc.com>
2017-07-04 07:52:27 -0400

    To resolve the issue caused by code checkins

--- jobs/UnitTest/unit_test.sh
+++ jobs/UnitTest/unit_test.sh
@@ -36,7 +36,11 @@ unit_test(){
     ./node_modules/.bin/istanbul report lcov
     npm install --save-dev mocha-sonar-reporter
     npm_package_config_mocha_sonar_reporter_classname="Tests_build.spec"
-    echo ${SUDO_PASSWORD} |sudo -S npm_package_config_mocha_sonar_reporter_outputfile=test/$1.xml ./node_modules/.bin/istanbul cover -x "**/spec/**" ./node_modules/.bin/_mocha -- $(find spec -name '*-spec.js') -R mocha-sonar-reporter --require spec/helper.js -t 10000
+    if [ $1 -eq "image-service"]; then
+        echo ${SUDO_PASSWORD} |sudo -S npm_package_config_mocha_sonar_reporter_outputfile=test/$1.xml ./node_modules/.bin/istanbul cover -x "**/spec/**" ./node_modules/.bin/_mocha -- $(find spec -name '*-spec.js') -R mocha-sonar-reporter --require spec/helper.js -t 10000
+    else
+        npm_package_config_mocha_sonar_reporter_outputfile=test/$1.xml ./node_modules/.bin/istanbul cover -x "**/spec/**" ./node_modules/.bin/_mocha -- $(find spec -name '*-spec.js') -R mocha-sonar-reporter --require spec/helper.js -t 10000
+    fi
     set -e
 }
 
@@ -45,5 +49,7 @@ prepare_deps $1
 pushd ${WORKSPACE}/build-deps/$1
 unit_test $1
 cp test/$1.xml ${WORKSPACE}/xunit-reports
-sudo chown -R ${USER}:${USER} test coverage static
+if [ $1 -eq "image-service"]; then
+    sudo chown -R ${USER}:${USER} test coverage static
+fi
 popd
