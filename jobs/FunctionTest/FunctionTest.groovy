4518f8b77a70e0e17e1eeef24cc78273a8b52590
sunnyzhang <sunny.zhang@emc.com>
2017-07-05 02:59:17 -0400

    Fix the error caused by checkin

--- jobs/FunctionTest/FunctionTest.groovy
+++ jobs/FunctionTest/FunctionTest.groovy
@@ -54,22 +54,21 @@ def functionTest(String test_name, String TEST_GROUP, Boolean RUN_FIT_TEST, Bool
             try{
                 timeout(90){
                     // run test script
-                    def repos = env.REPOS_UNDER_TEST.tokenize(',')
-                        if(repos.contains("image-service")){
-                            sh '''#!/bin/bash -x
-                                pushd build-config
-                                ./build-config image-service
-                                popd
-                                ./build-config/test.sh
-                                '''
-                        }
-                        else{
-                            sh '''#!/bin/bash -x
-                                pushd build-config
-                                ./build-config 
-                                popd
-                                ./build-config/test.sh
-                                '''
+                    if (env.REPOS_UNDER_TEST && env.REPOS_UNDER_TEST.tokenize(',').contains("image-service")) {
+                        sh '''#!/bin/bash -x
+                            pushd build-config
+                            ./build-config image-service
+                            popd
+                            ./build-config/test.sh
+                            '''
+                    }
+                    else{
+                        sh '''#!/bin/bash -x
+                            pushd build-config
+                            ./build-config
+                            popd
+                            ./build-config/test.sh
+                            '''
                         }
                 }
             } finally{
